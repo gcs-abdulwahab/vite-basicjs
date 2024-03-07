4e22327318c2968e0e2afe71eb37701270df43da
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-03-07 22:57:47 +0500

    joke
    joke api  fetch   async await    use Effect

--- src/CompB.jsx
+++ src/CompB.jsx
@@ -1,20 +1,30 @@
-import { useState, useEffect } from "react";
+import React, { useState ,  useEffect } from "react";
 
 const CompB = () => {
-	const [name, setName] = useState("");
+
+	const [joke, setJoke] = useState('...')
 
 	useEffect(() => {
-		console.log("useEffect  of Comp B");
-	});
+		console.log("Comp B Component");
+		
+		/* fetch('https://v2.jokeapi.dev/joke/Programming?type=single')
+			.then(response => response.json())
+			.then(data => setJoke(data.joke))
+	 */
+
+		let x = async () => {
+			let response = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single')
+			let data = await response.json()
+			setJoke(data.joke)
+		}
+		x()
+		
+	},[]);
 
 	return (
 		<div>
 			<h1> Comp B</h1>
-			<input
-				type="text"
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-			/>
+			<p>{joke}</p>
 		</div>
 	);
 };
